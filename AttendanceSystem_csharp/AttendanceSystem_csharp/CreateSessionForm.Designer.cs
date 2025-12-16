d23671fa85dc2f06df1a849a2c1657010de0bafe
yahybro10 <fuadyahia903@gmail.com>
2025-12-17 00:26:40 +0300

    Add Create Sesssion form
    added a form for creating session
    #commit by Yahia Fuad

---
+++ AttendanceSystem_csharp/AttendanceSystem_csharp/CreateSessionForm.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace AttendanceSystem_csharp
+{
+    partial class CreateSessionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateSessionForm));
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.txtGroup = new System.Windows.Forms.TextBox();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnCreateSession = new System.Windows.Forms.Button();
+            this.bgPicture = new System.Windows.Forms.PictureBox();
+            this.headerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(55)))));
+            this.headerPanel.Controls.Add(this.btnClose);
+            this.headerPanel.Controls.Add(this.lblTitle);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(900, 60);
+            this.headerPanel.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(848, 17);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(40, 40);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(307, 18);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(222, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Create New Session";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(267, 104);
+            this.txtTitle.MaxLength = 150;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(300, 31);
+            this.txtTitle.TabIndex = 1;
+            this.txtTitle.Text = "Session Title";
+            this.txtTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.txtCourse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCourse.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourse.Location = new System.Drawing.Point(267, 161);
+            this.txtCourse.MaxLength = 100;
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(300, 31);
+            this.txtCourse.TabIndex = 3;
+            this.txtCourse.Text = "Course Name";
+            this.txtCourse.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.txtLevel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLevel.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLevel.Location = new System.Drawing.Point(267, 217);
+            this.txtLevel.MaxLength = 50;
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(300, 30);
+            this.txtLevel.TabIndex = 4;
+            this.txtLevel.Text = "Level";
+            this.txtLevel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtGroup
+            // 
+            this.txtGroup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.txtGroup.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGroup.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGroup.Location = new System.Drawing.Point(267, 274);
+            this.txtGroup.MaxLength = 50;
+            this.txtGroup.Name = "txtGroup";
+            this.txtGroup.Size = new System.Drawing.Size(300, 31);
+            this.txtGroup.TabIndex = 5;
+            this.txtGroup.Text = "Group";
+            this.txtGroup.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dtStart
+            // 
+            this.dtStart.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dtStart.CalendarTitleBackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dtStart.CalendarTrailingForeColor = System.Drawing.SystemColors.ControlText;
+            this.dtStart.CustomFormat = "yyyy-MM-dd hh:mm tt";
+            this.dtStart.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtStart.Location = new System.Drawing.Point(267, 344);
+            this.dtStart.MaxDate = new System.DateTime(2027, 1, 1, 0, 0, 0, 0);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.ShowUpDown = true;
+            this.dtStart.Size = new System.Drawing.Size(300, 30);
+            this.dtStart.TabIndex = 6;
+            this.dtStart.Value = new System.DateTime(2025, 11, 30, 0, 0, 0, 0);
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.lblStart.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(371, 318);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(88, 23);
+            this.lblStart.TabIndex = 7;
+            this.lblStart.Text = "Start Time";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.lblEnd.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnd.Location = new System.Drawing.Point(371, 402);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(81, 23);
+            this.lblEnd.TabIndex = 8;
+            this.lblEnd.Text = "End Time";
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.CalendarFont = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtEnd.CustomFormat = "yyyy-MM-dd hh:mm tt";
+            this.dtEnd.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtEnd.Location = new System.Drawing.Point(267, 428);
+            this.dtEnd.MaxDate = new System.DateTime(2027, 1, 1, 0, 0, 0, 0);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.ShowUpDown = true;
+            this.dtEnd.Size = new System.Drawing.Size(300, 30);
+            this.dtEnd.TabIndex = 9;
+            this.dtEnd.Value = new System.DateTime(2025, 11, 30, 0, 0, 0, 0);
+            // 
+            // btnCreateSession
+            // 
+            this.btnCreateSession.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCreateSession.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(60)))), ((int)(((byte)(80)))));
+            this.btnCreateSession.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCreateSession.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateSession.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateSession.ForeColor = System.Drawing.Color.White;
+            this.btnCreateSession.Location = new System.Drawing.Point(352, 483);
+            this.btnCreateSession.Name = "btnCreateSession";
+            this.btnCreateSession.Size = new System.Drawing.Size(150, 40);
+            this.btnCreateSession.TabIndex = 10;
+            this.btnCreateSession.Text = "Create Session";
+            this.btnCreateSession.UseVisualStyleBackColor = false;
+            this.btnCreateSession.Click += new System.EventHandler(this.btnCreateSession_Click);
+            // 
+            // bgPicture
+            // 
+            this.bgPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bgPicture.Image = ((System.Drawing.Image)(resources.GetObject("bgPicture.Image")));
+            this.bgPicture.Location = new System.Drawing.Point(0, 0);
+            this.bgPicture.Name = "bgPicture";
+            this.bgPicture.Size = new System.Drawing.Size(900, 600);
+            this.bgPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bgPicture.TabIndex = 11;
+            this.bgPicture.TabStop = false;
+            this.bgPicture.Click += new System.EventHandler(this.bgPicture_Click);
+            // 
+            // CreateSessionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Controls.Add(this.btnCreateSession);
+            this.Controls.Add(this.dtEnd);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.dtStart);
+            this.Controls.Add(this.txtGroup);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.headerPanel);
+            this.Controls.Add(this.bgPicture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CreateSessionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CreateSessionForm";
+            this.Load += new System.EventHandler(this.CreateSessionForm_Load);
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.TextBox txtGroup;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Button btnCreateSession;
+        private System.Windows.Forms.PictureBox bgPicture;
+    }
+}
---
+++ AttendanceSystem_csharp/AttendanceSystem_csharp/CreateSessionForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AttendanceSystem_csharp
+{
+    public partial class CreateSessionForm : Form
+    {
+        public CreateSessionForm()
+        {
+            InitializeComponent();
+            AddCloseHover(btnClose);
+        }
+        private void AddCloseHover(Button btn)
+        {
+            btn.MouseEnter += (s, e) =>
+            {
+                btn.BackColor = Color.FromArgb(200, 50, 50);
+            };
+            btn.MouseLeave += (s, e) => { btn.BackColor = Color.FromArgb(30, 40, 55); };
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CreateSessionForm_Load(object sender, EventArgs e)
+        {
+            SetPlaceholder(txtTitle, "Session Title");
+            SetPlaceholder(txtCourse, "Course Name");
+            SetPlaceholder(txtLevel, "Level");
+            SetPlaceholder(txtGroup, "Group");
+            AddDatePickerEffects(dtStart);
+            AddDatePickerEffects(dtEnd);
+            AddHoverEffect(btnCreateSession);
+        }
+        private void SetPlaceholder(TextBox txt, string placeholder)
+        {
+            if (string.IsNullOrWhiteSpace(txt.Text))
+            {
+                txt.Text = placeholder;
+                txt.ForeColor = Color.Gray;
+            }
+            txt.Enter += (s, e) =>
+            {
+                if (txt.Text == placeholder) {
+                    txt.Text = "";
+                    txt.ForeColor = Color.Black;
+                }
+                txt.BackColor = Color.FromArgb(240, 248, 255);
+            };
+            txt.Leave += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace (txt.Text))
+                {
+                    txt.Text = placeholder;
+                    txt.ForeColor = Color.Gray;
+                }
+                txt.BackColor = Color.White;
+            };
+
+            }
+        private void AddDatePickerEffects (DateTimePicker dt)
+        {
+            dt.Enter += (s, e) =>
+            {
+                dt.CalendarMonthBackground = Color.FromArgb(240, 248, 255);
+                dt.CalendarTitleBackColor = Color.FromArgb(240, 248, 255);
+            };
+            dt.Leave += (s, e) =>
+            {
+                dt.CalendarMonthBackground = Color.White;
+                dt.CalendarTitleBackColor = Color.White;
+            };
+        }
+        private void AddHoverEffect(Button btn)
+        {
+            btn.MouseEnter += (s, e) =>
+            {
+                btn.BackColor = Color.FromArgb(65, 75, 100);
+            };
+            btn.MouseLeave += (s, e) =>
+            {
+                btn.BackColor = Color.FromArgb(50, 60, 80);
+            };
+        }
+
+        private void btnCreateSession_Click(object sender, EventArgs e)
+        {
+          
+            try
+            {
+                string title = txtTitle.Text.Trim();
+                string course = txtCourse.Text.Trim();
+                string level = txtLevel.Text.Trim();
+                string group = txtGroup.Text.Trim();
+                DateTime start = dtStart.Value;
+                DateTime end = dtEnd.Value;
+                DateTime now = DateTime.Now;
+                
+                if (string.IsNullOrWhiteSpace(title) || title == "Session Title" || string.IsNullOrWhiteSpace(course) || course == "Course Name" ||
+                    string.IsNullOrWhiteSpace(level) || level == "Level" || string.IsNullOrEmpty(group) || group == "Group")
+                {
+                    MessageBox.Show("فضلا املا جميع الحقول قبل المتابعة");
+                    return;
+                }
+                if(start>=end)
+                {
+                    MessageBox.Show("وقت البداية يجب ان يكون قبل وقت النهاية");
+                    return;
+                }
+                if(start < now)
+                {
+                    MessageBox.Show("لا يمكن ان يكون وقت بداية الجلسة اقل من الوقت الحالي");
+                    return;
+                }
+                if(start.Date!=end.Date)
+                {
+                    MessageBox.Show("يجب ان يكون وقت بداية الجلسة ووقت نهايتها في نفس اليوم");
+                    return;
+                }
+                string token = Guid.NewGuid().ToString("N").Substring(0, 12); //هذه الدالة تنشى قيمة عالمية فريدة
+                string query = "INSERT INTO sessions (title, course_name, `level`, group_name, start_time, end_time, token) " +
+                    "VALUES (@title, @course, @level, @group, @start, @end, @token)";
+                // نمرر القيم بامان عبر المعاملات
+                int rows = DB.ExecuteNonQuery(query,DB.Param("@title", title), DB.Param("@course", course), DB.Param("@level", level),
+                   DB.Param("@group", group), DB.Param("@start", start), DB.Param("@end", end), DB.Param("@token", token));
+                if(rows > 0)
+                {
+                    MessageBox.Show("تم انشاء الجلسة بنجاح");
+                    this.Close();
+                }
+            } catch (Exception ex)
+            {
+                DB.LogError(ex);
+                MessageBox.Show("حدث خطا اثناء انشاء الجلسة \n");
+            }
+
+        }
+
+        private void bgPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
