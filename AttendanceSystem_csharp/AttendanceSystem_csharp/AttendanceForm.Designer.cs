dab1e5e602e7e3fb3bcff65ea95267a6593d64b8
yahybro10 <fuadyahia903@gmail.com>
2025-12-17 00:40:50 +0300

    Add Attendance form
    added a form of attendance that show the  information students
    #commit by Yahia Fuad

---
+++ AttendanceSystem_csharp/AttendanceSystem_csharp/AttendanceForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AttendanceSystem_csharp
+{
+    partial class AttendanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AttendanceForm));
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.attendanceGrid = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.bgPicture = new System.Windows.Forms.PictureBox();
+            this.headerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.attendanceGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(55)))));
+            this.headerPanel.Controls.Add(this.btnClose);
+            this.headerPanel.Controls.Add(this.lblTitle);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(950, 60);
+            this.headerPanel.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(898, 17);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(40, 40);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(371, 23);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(121, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Attendance";
+            // 
+            // attendanceGrid
+            // 
+            this.attendanceGrid.AllowUserToAddRows = false;
+            this.attendanceGrid.AllowUserToDeleteRows = false;
+            this.attendanceGrid.BackgroundColor = System.Drawing.Color.White;
+            this.attendanceGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.attendanceGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.attendanceGrid.Location = new System.Drawing.Point(22, 130);
+            this.attendanceGrid.Name = "attendanceGrid";
+            this.attendanceGrid.ReadOnly = true;
+            this.attendanceGrid.RowHeadersWidth = 51;
+            this.attendanceGrid.RowTemplate.Height = 24;
+            this.attendanceGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.attendanceGrid.Size = new System.Drawing.Size(850, 340);
+            this.attendanceGrid.TabIndex = 1;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(392, 509);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 25);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.Text = "Search...";
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // lblCount
+            // 
+            this.lblCount.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(433, 85);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(150, 23);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "عدد الطلاب:";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCount.TextChanged += new System.EventHandler(this.lblCount_TextChanged);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(55)))));
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.ForeColor = System.Drawing.Color.White;
+            this.btnRefresh.Location = new System.Drawing.Point(704, 79);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(80, 35);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // bgPicture
+            // 
+            this.bgPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bgPicture.Image = ((System.Drawing.Image)(resources.GetObject("bgPicture.Image")));
+            this.bgPicture.Location = new System.Drawing.Point(0, 0);
+            this.bgPicture.Name = "bgPicture";
+            this.bgPicture.Size = new System.Drawing.Size(950, 600);
+            this.bgPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bgPicture.TabIndex = 5;
+            this.bgPicture.TabStop = false;
+            // 
+            // AttendanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(950, 600);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.attendanceGrid);
+            this.Controls.Add(this.headerPanel);
+            this.Controls.Add(this.bgPicture);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AttendanceForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AttendanceForm";
+            this.Load += new System.EventHandler(this.AttendanceForm_Load);
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.attendanceGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView attendanceGrid;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.PictureBox bgPicture;
+    }
+}
---
+++ AttendanceSystem_csharp/AttendanceSystem_csharp/AttendanceForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AttendanceSystem_csharp
+{
+    public partial class AttendanceForm : Form
+    {
+        private int sessionId;
+        private DataTable attendanceTable;
+        public AttendanceForm(int sessionId)
+        {
+            InitializeComponent();
+            this.sessionId = sessionId;
+            attendanceGrid.AutoGenerateColumns = false;
+            CreateColumns();
+            SetSearchPlaceholder();
+            LoadAttendance();
+            AddCloseHover(btnClose);
+        }
+        private void AddCloseHover(Button btn)
+        {
+            btn.MouseEnter += (s, e) =>
+            {
+                btn.BackColor = Color.FromArgb(200, 50, 50);
+            };
+            btn.MouseLeave += (s, e) => { btn.BackColor = Color.FromArgb(30, 40, 55); };
+        }
+
+        private void AttendanceForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadAttendance()
+        {
+            try
+            {
+                string query = "SELECT id, student_id, student_name, created_at FROM attendance WHERE session_id = @id ORDER BY created_at DESC";
+                DataTable dt = DB.ExecuteQuery(query, DB.Param("@id", sessionId));
+                attendanceTable = dt;
+                attendanceTable.Columns.Add("student_name_lower", typeof(string));
+                attendanceTable.Columns.Add("student_id_lower", typeof(string));
+                foreach (DataRow row in attendanceTable.Rows)
+                {
+                    row["student_name_lower"] = row["student_name"].ToString().ToLower();
+                    row["student_id_lower"] = row["student_id"].ToString().ToLower();
+                }
+                attendanceGrid.DataSource = attendanceTable;
+                lblCount.Text = "عدد الطلاب: " + attendanceTable.DefaultView.Count;
+            } catch (Exception ex)
+            {
+                DB.LogError(ex);
+                MessageBox.Show("حدث خطا اثناء تحميل بيانات الحضور");
+            }
+        }
+        private void CreateColumns()
+        {
+            attendanceGrid.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colID",
+                HeaderText = "ID",
+                DataPropertyName = "id",
+                Width = 60
+            });
+            attendanceGrid.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colStudentName",
+                HeaderText = "Student Name",
+                DataPropertyName = "student_name",
+                Width = 220
+            });
+            attendanceGrid.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colStudentId",
+                HeaderText = "Student ID",
+                DataPropertyName = "student_id",
+                Width = 150
+            });
+            attendanceGrid.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "colTime",
+                HeaderText = "Time",
+                DataPropertyName = "created_at",
+                Width = 170
+            });
+        }
+        private void SetSearchPlaceholder()
+        {
+            if (string.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                txtSearch.Text = "Search...";
+                txtSearch.ForeColor = Color.Gray;
+            }
+            txtSearch.Enter += (s, e) =>
+            {
+                if (txtSearch.Text == "Search...")
+                {
+                    txtSearch.Text = "";
+                    txtSearch.ForeColor = Color.Black;
+                }
+            };
+            txtSearch.Leave += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(txtSearch.Text))
+                {
+                    txtSearch.Text = "Search...";
+                    txtSearch.ForeColor = Color.Gray;
+                }
+            };
+        }
+        private void FilterAttendance()
+        {
+            if (attendanceTable == null)
+                return;
+            string keyword = txtSearch.Text.Trim();
+            if(string.IsNullOrWhiteSpace(keyword) || keyword == "Search...")
+            {
+                attendanceTable.DefaultView.RowFilter = "";
+                lblCount.Text = "عدد الطلاب: "+ attendanceTable.DefaultView.Count;
+                return;
+            }
+            string lowerKeyword = keyword.ToLower();
+            string safeKeyword = lowerKeyword.Replace("'", "''");
+            attendanceTable.DefaultView.RowFilter = $"student_name_lower LIKE '%{safeKeyword}%' OR " + $"student_id_lower LIKE '%{safeKeyword}%'";
+            lblCount.Text = "عدد الطلاب: " + attendanceTable.DefaultView.Count;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadAttendance();
+            if(!string.IsNullOrWhiteSpace(txtSearch.Text)&& txtSearch.Text != "Search...")
+            {
+                FilterAttendance();
+            } 
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lblCount_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            FilterAttendance();
+        }
+    }
+}
